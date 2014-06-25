8fd26cd11fe4301915c06664b982936b172eaa58
Glenn Ansley <glenn@ithemes.com>
2014-06-25 07:52:17 -0500

    Updating ownership info

--- exchange-addon-madmimi.php
+++ exchange-addon-madmimi.php
@@ -12,14 +12,16 @@
  * Plugin Name:  iThemes Exchange - MadMimi Add-on
  * Plugin URI:   http://ithemes.com/exchange/madmimi/
  * Description:  Integrates Mad Mimi into the iThemes Exchange plugin.
- * Version:      1.0.4
- * Author:       Thomas Griffin
- * Author URI:   http://thomasgriffinmedia.com/
+ * Version:      1.0.5
+ * Author:       iThemes
+ * Author URI:   http://ithemes.com/exchange
  * Text Domain:  LION
- * Contributors: griffinjt
+ * Contributors: ithemes, griffinjt
  * License:      GPL-2.0+
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:  /lang
+ *
+ * This add-on was originally developed by Thomas Griffin <http://thomasgriffinmedia.com/>
  */
 
 // If this file is called directly, abort.
@@ -66,8 +68,8 @@ function tgm_exchange_madmimi_register() {
         $options = array(
             'name'              => __( 'MadMimi', 'tgm-exchange-madmimi' ),
             'description'       => __( 'Adds a MadMimi optin checkbox to the user registration form.', 'tgm-exchange-madmimi' ),
-            'author'            => 'Thomas Griffin',
-            'author_url'        => 'http://thomasgriffinmedia.com/',
+            'author'            => 'iThemes',
+            'author_url'        => 'http://ithemes.com/exchange/',
             'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/madmimi50px.png' ),
             'file'              => dirname( __FILE__ ) . '/class-exchange-addon-madmimi.php',
             'category'          => 'email',
