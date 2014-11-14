36b8317e12e2e478ae27f66b7bf7fc9384c60628
root <root@petra.ithemes.com>
2014-11-14 10:13:26 -0600

    1.0.7: updated: lib/updater

--- exchange-addon-madmimi.php
+++ exchange-addon-madmimi.php
@@ -12,7 +12,7 @@
  * Plugin Name:  iThemes Exchange - MadMimi Add-on
  * Plugin URI:   http://ithemes.com/exchange/madmimi/
  * Description:  Integrates Mad Mimi into the iThemes Exchange plugin.
- * Version:      1.0.6
+ * Version:      1.0.7
  * Author:       iThemes
  * Author URI:   http://ithemes.com/exchange
  * Text Domain:  LION
